a2878a25f40b462908238fc12858a47909db3d07
carlos93 <carlos93_fuenla@hotmail.com>
2012-04-14 19:30:32 +0200

    [mr1892] Fix some achievements criterias. Thanks Romman.

---
+++ sql_mr/mr1892_mangos_achievements_criteria.sql
@@ -0,0 +1,42 @@
+replace into achievement_criteria_requirement values(5512,0,0,0);
+replace into achievement_criteria_requirement values(5529,0,0,0);
+replace into achievement_criteria_requirement values(5530,0,0,0);
+replace into achievement_criteria_requirement values(5531,0,0,0);
+replace into achievement_criteria_requirement values(5532,0,0,0);
+
+replace into achievement_criteria_requirement values(5533,0,0,0);
+replace into achievement_criteria_requirement values(5534,0,0,0);
+replace into achievement_criteria_requirement values(5535,0,0,0);
+replace into achievement_criteria_requirement values(9165,0,0,0);
+replace into achievement_criteria_requirement values(9166,0,0,0);
+
+replace into achievement_criteria_requirement values(5536,0,0,0);
+replace into achievement_criteria_requirement values(5537,0,0,0);
+replace into achievement_criteria_requirement values(5538,0,0,0);
+replace into achievement_criteria_requirement values(5539,0,0,0);
+replace into achievement_criteria_requirement values(5540,0,0,0);
+replace into achievement_criteria_requirement values(13254,0,0,0);
+
+replace into achievement_criteria_requirement values(5541,0,0,0);
+replace into achievement_criteria_requirement values(5542,0,0,0);
+replace into achievement_criteria_requirement values(5543,0,0,0);
+
+replace into achievement_criteria_requirement values(5544,6,2597,0);
+replace into achievement_criteria_requirement values(5545,6,3358,0);
+replace into achievement_criteria_requirement values(5546,6,3277,0);
+replace into achievement_criteria_requirement values(5547,6,3820,0);
+replace into achievement_criteria_requirement values(5548,6,4384,0);
+replace into achievement_criteria_requirement values(13255,6,4710,0);
+
+replace into achievement_criteria_requirement values(12178,6,4710,0);
+replace into achievement_criteria_requirement values(12179,6,4710,0);
+replace into achievement_criteria_requirement values(12181,6,4710,0);
+replace into achievement_criteria_requirement values(12182,6,4710,0);
+
+replace into achievement_criteria_requirement values(11497,6,4710,0);
+replace into achievement_criteria_requirement values(11498,6,4710,0);
+replace into achievement_criteria_requirement values(11500,6,4710,0);
+replace into achievement_criteria_requirement values(11501,6,4710,0);
+
+delete from achievement_criteria_requirement where criteria_id in(7625,7628);
+insert into achievement_criteria_requirement values(7625,0,0,0),(7628,0,0,0);
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "1891"
+ #define REVISION_R2 "1892"
 #endif // __REVISION_R2_H__
