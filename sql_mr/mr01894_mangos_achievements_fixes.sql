2d66796b729f069c3b5d8ea0c26ec2ff32f39932
Undergarun <undergarun@gmail.com>
2012-04-14 21:21:46 +0200

    [mr1894] Move achievement criterias from [mr1892] to the same achievement fixes file. Let's attempt to keep as better ordered as possible.
    Also if someone can check proper criteria type for achievements 7625, 7628 and know right value, let me know.

--- sql_mr/mr01894_mangos_achievements_fixes.sql
+++ sql_mr/mr01894_mangos_achievements_fixes.sql
@@ -1,12 +1,14 @@
--- achievement works ( some stuff will be added and fixed  ---- !!!( deletetion can be out of number order but pls keep insert in number order )!!!
+-- Achievements works (Please keep deletion and insert in right order)
 
-DELETE FROM achievement_criteria_requirement WHERE criteria_id IN 
-(608,609,610,1242,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,1815,1816,1817,1818,1819,1820,1821,1822,1823,1825,1826,4227,5726,5727,5728,5734,5735,5736,5737,5738,5739,5809,5810,7020,7625,7628,8587,8588,8589,8590,8599,8600,8601,8602,8607,8608,8609,8610,8615,8616,8617,8618,8759,8758);
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN
+(608,609,610,1242,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,1815,1816,1817,1818,1819,1820,1821,
+1822,1823,1825,1826,4227,5512,5529,5530,5531,5532,5533,5534,5535,5536,5537,5538,5539,5540,5541,5542,5543,5544,5545,
+5546,5547,5548,5726,5727,5728,5734,5735,5736,5737,5738,5739,5809,5810,7020,7625,7628,8587,8588,8589,8590,8599,8600,8601,
+8602,8607,8608,8609,8610,8615,8616,8617,8618,8759,8758,9165,9166,11497,11498,11500,11501,12178,12179,12181,12182,13254,13255);
 
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (608, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (609, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (610, 11);
-
 
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1242, 11);
 
@@ -37,6 +39,32 @@ INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (4227, 15, 3);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (4227, 5, 26682);
 
+INSERT INTO `achievement_criteria_requirement` VALUES (5512,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5529,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5530,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5531,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5532,0,0,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (5533,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5534,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5535,0,0,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (5536,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5537,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5538,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5539,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5540,0,0,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (5541,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5542,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5543,0,0,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (5544,6,2597,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5545,6,3358,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5546,6,3277,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5547,6,3820,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (5548,6,4384,0);
+
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (5726, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (5727, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (5728, 11);
@@ -55,8 +83,8 @@ INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (5
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (7020, 6, 4571);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (7020, 7, 23335);
 
-INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (7625, 11);
-INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (7628, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (7625, 0); -- Must be type 11 ???
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (7628, 0); -- Must be type 11 ???
 
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (8587, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (8588, 11);
@@ -80,3 +108,18 @@ INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (8
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (8759, 0);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (8758, 0);
 
+INSERT INTO `achievement_criteria_requirement` VALUES (9165,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (9166,0,0,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (11497,6,4710,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (11498,6,4710,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (11500,6,4710,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (11501,6,4710,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (12178,6,4710,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (12179,6,4710,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (12181,6,4710,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (12182,6,4710,0);
+
+INSERT INTO `achievement_criteria_requirement` VALUES (13254,0,0,0);
+INSERT INTO `achievement_criteria_requirement` VALUES (13255,6,4710,0);
