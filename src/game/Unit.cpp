a16751d8915191d36a2ead32db952babae1f4ff7
rsa <kris13@bk.ru>
2012-04-30 20:22:27 +0400

    [mr1952] Revert "[001934] Prevent civilian creatures catch threat list."
    This reverts commit 2aacde4874b028e88a52e7798d3bc553813f36fa.
    Wrong.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9881,10 +9881,6 @@ bool Unit::CanHaveThreatList() const
     if (creature->IsTotem())
         return false;
 
-    // Civilian creatures can not have a threat list
-    if (creature->IsCivilian())
-        return false;
-
     // pets can not have a threat list, unless they are controlled by a creature
     if (creature->IsPet() && creature->GetOwnerGuid().IsPlayer())
         return false;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "1951"
+ #define REVISION_R2 "1952"
 #endif // __REVISION_R2_H__
