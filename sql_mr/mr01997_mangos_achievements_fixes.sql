058ae08e08bd9e6e9efd8bda7b2a35ebf48471aa
Romman <s.roma@tut.by>
2012-05-13 01:29:57 +0200

    [mr1997] Fix achievements 204 and 216. Related to #1005

--- sql_mr/mr01997_mangos_achievements_fixes.sql
+++ sql_mr/mr01997_mangos_achievements_fixes.sql
@@ -1,8 +1,8 @@
 -- Achievements works (Please keep deletion and insert in right order)
 
 DELETE FROM achievement_criteria_requirement WHERE criteria_id IN
-(608,609,610,1242,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,1815,1816,1817,1818,1819,1820,1821,
-1822,1823,1825,1826,4227,5512,5529,5530,5531,5532,5533,5534,5535,5536,5537,5538,5539,5540,5541,5542,5543,5544,5545,
+(608,609,610,1242,1802,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,1815,1816,1817,1818,1819,1820,1821,
+1822,1823,1824,1825,1826,3353,4227,5512,5529,5530,5531,5532,5533,5534,5535,5536,5537,5538,5539,5540,5541,5542,5543,5544,5545,
 6641,6642,6643,6644,6651,6652,6653,6654,6655,6656,6657,6659,
 5546,5547,5548,5726,5727,5728,5734,5735,5736,5737,5738,5739,5809,5810,7020,7625,7628,8587,8588,8589,8590,8599,8600,8601,
 8602,8607,8608,8609,8610,8615,8616,8617,8618,8759,8758,9165,9166,10391,11497,11498,11500,11501,12178,12179,12181,12182,12398,13254,13255);
@@ -13,6 +13,7 @@ INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (6
 
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1242, 11);
 
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1802, 0);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1803, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1804, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1805, 11);
@@ -34,8 +35,11 @@ INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1821, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1822, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1823, 11);
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1824, 0);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1825, 11);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (1826, 11);
+
+INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`) VALUES (3353, 0);
 
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (4227, 15, 3);
 INSERT INTO `achievement_criteria_requirement` (`criteria_id`, `type`, `value1`) VALUES (4227, 5, 26682);
--- src/game/AchievementMgr.cpp
+++ src/game/AchievementMgr.cpp
@@ -2343,7 +2343,7 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                                                          // WS, capture 3 flags without dying
                         if (achievementCriteria->referredAchievement == 204)
                         {
-                            if (!(bg->GetPlayerScore(GetPlayer(), SCORE_DEATHS) == 0 && bg->GetPlayerScore(GetPlayer(), SCORE_FLAG_CAPTURES) >= 3))
+                            if (bg->GetPlayerScore(GetPlayer(), SCORE_DEATHS))
                                 continue;
                         }
                         break;
@@ -2385,7 +2385,7 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                             }
                             case 216:                    // EY, capture 3 flags without dying
                             {
-                                if (!(bg->GetPlayerScore(GetPlayer(), SCORE_DEATHS) == 0 && bg->GetPlayerScore(GetPlayer(), SCORE_FLAG_CAPTURES) >= 3))
+                                if (bg->GetPlayerScore(GetPlayer(), SCORE_DEATHS))
                                     continue;
                                 break;
                             }
