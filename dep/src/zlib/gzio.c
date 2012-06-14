6c370462c8649450e9244bff41814b7a909c4a73
rsa <kris13@bk.ru>
2012-06-14 21:52:40 +0400

    [mr2026] some corrects to utility subsystems (required for dynamic_vmaps)

--- dep/src/zlib/gzio.c
+++ dep/src/zlib/gzio.c
@@ -9,6 +9,8 @@
 
 #include <stdio.h>
 
+#include <errno.h>
+
 #include "zutil.h"
 
 #ifdef NO_DEFLATE       /* for compatibility with old definition */
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "2025"
+ #define REVISION_R2 "2026"
 #endif // __REVISION_R2_H__
