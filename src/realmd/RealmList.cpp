81d13ab94df697ce663ccc58f53538bb40d771aa
rsa <kris13@bk.ru>
2012-08-26 18:52:34 +0400

    [mr2197] allow 4.4.3, 4.3.0 and 4.0.6a connect to realmd

--- src/realmd/RealmList.cpp
+++ src/realmd/RealmList.cpp
@@ -35,8 +35,12 @@ extern DatabaseType LoginDatabase;
 // if you need more from old build then add it in cases in realmd sources code
 // list sorted from high to low build and first build used as low bound for accepted by default range (any > it will accepted by realmd at least)
 
-static RealmBuildInfo ExpectedRealmdClientBuilds[] = {
-    {12340, 3, 3, 5, 'a'},                                  // highest supported build, also auto accept all above for simplify future supported builds testing
+static RealmBuildInfo ExpectedRealmdClientBuilds[] =
+{
+    {15595, 4, 3, 4, ' '},                                  // highest supported build, also auto accept all above for simplify future supported builds testing
+    {15050, 4, 3, 0, ' '},
+    {13623, 4, 0, 6, 'a'},
+    {12340, 3, 3, 5, 'a'},
     {11723, 3, 3, 3, 'a'},
     {11403, 3, 3, 2, ' '},
     {11159, 3, 3, 0, 'a'},
