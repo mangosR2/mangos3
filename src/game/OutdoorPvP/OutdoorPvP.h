fdae972e31986fa4120753e56c80d54df11a8df6
zerg <Sergey.Chelombitko@gmail.com>
2012-09-02 12:36:48 +0600

    [12142] Fix possible memory leaks in Outdoor PvP

--- src/game/OutdoorPvP/OutdoorPvP.h
+++ src/game/OutdoorPvP/OutdoorPvP.h
@@ -53,7 +53,7 @@ class OutdoorPvP
 
     public:
         OutdoorPvP() {}
-        ~OutdoorPvP() {}
+        virtual ~OutdoorPvP() {}
 
         // called when the zone is initialized
         virtual void FillInitialWorldStates(uint32 zoneId) {}
