395271cb1ccbdc0b3e52219202af4c1c58f3f483
rsa <kris13@bk.ru>
2012-09-12 10:17:00 +0400

    [mr2292] not check broken DBC data while doing spellmgr check. close #1161

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -334,6 +334,9 @@ inline bool IsSpellWithCasterSourceTargetsOnly(SpellEntry const* spellInfo)
 {
     for(int i = 0; i < MAX_EFFECT_INDEX; ++i)
     {
+        if (spellInfo->Effect[i] == SPELL_EFFECT_NONE)
+            continue;
+
         uint32 targetA = spellInfo->EffectImplicitTargetA[i];
         if (targetA && !IsCasterSourceTarget(targetA))
             return false;
