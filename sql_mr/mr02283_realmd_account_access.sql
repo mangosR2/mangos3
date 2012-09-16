9b31b9abe2c7a3f0ed851539125cc4f112cbfdee
Undergarun <undergarun@gmail.com>
2012-09-16 23:40:20 +0200

    [mr2319] Remove trash from 2283 sql

--- sql_mr/mr02283_realmd_account_access.sql
+++ sql_mr/mr02283_realmd_account_access.sql
@@ -9,12 +9,6 @@ CREATE TABLE `account_access` (
   PRIMARY KEY (`id`,`RealmID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Account Access System';
 
-INSERT INTO `account_access` VALUES
-(1,3,-1),
-(2,2,-1),
-(3,1,-1),
-(4,0,-1);
-
 INSERT IGNORE INTO `account_access` (`id`,`gmlevel`,`RealmID`) 
     SELECT `id`, `gmlevel`, -1 FROM `account` 
     WHERE `account`.`gmlevel` > 0;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "2318"
+ #define REVISION_R2 "2319"
 #endif // __REVISION_R2_H__
