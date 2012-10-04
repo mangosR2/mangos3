95d72425aa6dd1e98ddd9971fe408eda354ad097
rsa <kris13@bk.ru>
2012-10-04 16:56:18 +0400

    [mr2351] fix some SQL's - OPvP ZM gossips not need after YTDB 627 (need restore if broken) - remove some SQL corrects (use structure changes instead) close #1186, #1027

--- sql_mr/mr01982_mangos_spell_hacks_rename.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE spell_dbc CHANGE ExcludeCasterAuraState CasterAuraStateNot int;
-ALTER TABLE spell_dbc CHANGE ExcludeTargetAuraState TargetAuraStateNot int;
-ALTER TABLE spell_dbc CHANGE ManaCostPct ManaCostPercentage int;
