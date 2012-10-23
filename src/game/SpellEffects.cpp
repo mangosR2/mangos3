167050b7f07e2867efc0241f873255ed46e6e30f
rsa <kris13@bk.ru>
2012-10-23 09:09:57 +0400

    [mr2385] small style fixes in 2384

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -9891,11 +9891,13 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     break;
                 }
                 case 62705:                                 // Auto-repair (Ulduar: RX-214)
-                    if (!unitTarget)
+                {
+                    if (!unitTarget || unitTarget->GetObjectGuid().IsVehicle())
                         return;
 
-                    unitTarget->SetHealthPercent(100.0f);
+                    unitTarget->SetHealthPercent(m_spellInfo->CalculateSimpleValue(eff_idx));
                     break;
+                }
                 case 62707:                                 // Grab (Ulduar: Ignis)
                 case 63535:                                 // Grab heroic
                 {
