b770c832c25f5fa5ff4e9ac80e9c5ee14bbe6174
rsa <kris13@bk.ru>
2012-11-05 18:45:51 +0400

    [mr2404] SQL fixes

--- sql_mr/custom_characters_tables.sql
+++ sql_mr/custom_characters_tables.sql
@@ -1,9 +1,9 @@
 -- Random Battleground
--- Commit 0525ca144e282cec2478
+--
 
 DROP TABLE IF EXISTS `character_battleground_random`;
 CREATE TABLE `character_battleground_random` (
-    `guid` int(11) unsigned NOT NULL default 0,
+    `guid` int(11) unsigned NOT NULL DEFAULT '0',
     PRIMARY KEY  (`guid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
