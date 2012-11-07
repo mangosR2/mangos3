9d596b17a0f5c37b58ecc50562d24e337f40bb35
Schmoozerd <schmoozerd@scriptdev2.com>
2012-11-07 18:54:15 +0100

    [12250] StartEvents_Event wrapper to start events
    Conflicts:
    	src/game/GameObject.cpp
    	src/shared/revision_nr.h

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -1161,10 +1161,8 @@ void GameObject::Use(Unit* user)
             // TODO: possible must be moved to loot release (in different from linked triggering)
             if (GetGOInfo()->chest.eventId)
             {
-                DEBUG_LOG("Chest ScriptStart id %u for GO %u", GetGOInfo()->chest.eventId, GetGUIDLow());
-
-                if (!sScriptMgr.OnProcessEvent(GetGOInfo()->chest.eventId, user, this, true))
-                    GetMap()->ScriptsStart(sEventScripts, GetGOInfo()->chest.eventId, user, this);
+                DEBUG_LOG("Chest ScriptStart id %u for %s (opened by %s)", GetGOInfo()->chest.eventId, GetGuidStr().c_str(), user->GetGuidStr().c_str());
+                StartEvents_Event(GetMap(), GetGOInfo()->chest.eventId, user, this);
             }
 
             return;
@@ -1314,11 +1312,10 @@ void GameObject::Use(Unit* user)
 
                 if (info->goober.eventId)
                 {
-                    DEBUG_FILTER_LOG(LOG_FILTER_AI_AND_MOVEGENSS, "Goober ScriptStart id %u for GO entry %u (GUID %u).", info->goober.eventId, GetEntry(), GetGUIDLow());
+                    DEBUG_FILTER_LOG(LOG_FILTER_AI_AND_MOVEGENSS, "Goober ScriptStart id %u for %s (Used by %s).", info->goober.eventId, GetGuidStr().c_str(), player->GetGuidStr().c_str());
+                    StartEvents_Event(GetMap(), info->goober.eventId, player, this);
 
-                    if (!sScriptMgr.OnProcessEvent(info->goober.eventId, player, this, true))
-                        GetMap()->ScriptsStart(sEventScripts, info->goober.eventId, player, this);
-
+                    // FIXME: hack for SOTA, need use regular scripts.
                     if (player->CanUseBattleGroundObject())
                     {
                         if (BattleGround *bg = player->GetBattleGround())
@@ -1387,10 +1384,7 @@ void GameObject::Use(Unit* user)
                 player->SendCinematicStart(info->camera.cinematicId);
 
             if (info->camera.eventID)
-            {
-                if (!sScriptMgr.OnProcessEvent(info->camera.eventID, player, this, true))
-                    GetMap()->ScriptsStart(sEventScripts, info->camera.eventID, player, this);
-            }
+                StartEvents_Event(GetMap(), info->camera.eventID, player, this);
 
             return;
         }
@@ -1796,7 +1790,7 @@ void GameObject::DamageTaken(Unit* pDoneBy, int32 damage, uint32 spellId)
     if (damage > 0 && IsFriendlyTo(pDoneBy))
         return;
 
-    Player* pWho = pDoneBy->GetCharmerOrOwnerPlayerOrPlayerItself();
+    Player* pWho = pDoneBy ? pDoneBy->GetCharmerOrOwnerPlayerOrPlayerItself() : NULL;
 
     DEBUG_FILTER_LOG(LOG_FILTER_DAMAGE, "GameObject::DamageTaken  damage taken: %i to health %u", damage, m_health);
 
@@ -1831,7 +1825,7 @@ void GameObject::DamageTaken(Unit* pDoneBy, int32 damage, uint32 spellId)
 
         // Start Event if exist
         if (pWho && GetGOInfo()->destructibleBuilding.intactEvent)
-            GetMap()->ScriptsStart(sEventScripts, GetGOInfo()->destructibleBuilding.intactEvent, this, pWho);
+            StartEvents_Event(GetMap(), GetGOInfo()->destructibleBuilding.intactEvent, this, pWho);
 
         SetLinkedWorldState(OBJECT_STATE_LAST_INDEX - (GetTeamIndex(GetTeam()) + 1)*OBJECT_STATE_PERIOD + OBJECT_STATE_INTACT);
         DEBUG_FILTER_LOG(LOG_FILTER_DAMAGE, "GameObject::DamageTaken %s gain INTACT state, team %u", GetObjectGuid().GetString().c_str(),GetTeam());
@@ -1844,8 +1838,8 @@ void GameObject::DamageTaken(Unit* pDoneBy, int32 damage, uint32 spellId)
             RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED);
             SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DESTROYED);
             SetDisplayId(GetGOInfo()->destructibleBuilding.destroyedDisplayId);
-            if (GetGOInfo()->destructibleBuilding.destroyedEvent);
-                GetMap()->ScriptsStart(sEventScripts, GetGOInfo()->destructibleBuilding.destroyedEvent, this, pWho);
+            if (pWho && GetGOInfo()->destructibleBuilding.destroyedEvent);
+                StartEvents_Event(GetMap(), GetGOInfo()->destructibleBuilding.destroyedEvent, this, pWho);
 
             if (pWho)
             {
@@ -1865,8 +1859,8 @@ void GameObject::DamageTaken(Unit* pDoneBy, int32 damage, uint32 spellId)
             SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED);
             SetDisplayId(GetGOInfo()->destructibleBuilding.damagedDisplayId);
 
-            if (GetGOInfo()->destructibleBuilding.damageEvent);
-                GetMap()->ScriptsStart(sEventScripts, GetGOInfo()->destructibleBuilding.damageEvent, this, pWho);
+            if (pWho && GetGOInfo()->destructibleBuilding.damageEvent);
+                StartEvents_Event(GetMap(), GetGOInfo()->destructibleBuilding.damageEvent, this, pWho);
 
             // if we have a "dead" display we can "kill" the building after its damaged
             if (GetGOInfo()->destructibleBuilding.destroyedDisplayId)
@@ -1902,8 +1896,8 @@ void GameObject::Rebuild(Unit* pCaster)
     SetDisplayId(GetGOInfo()->displayId);
     m_health = GetMaxHealth();
 
-    if (GetGOInfo()->destructibleBuilding.rebuildingEvent)
-        GetMap()->ScriptsStart(sEventScripts, GetGOInfo()->destructibleBuilding.rebuildingEvent, this, pCaster->GetCharmerOrOwnerOrSelf());
+    if (pCaster && GetGOInfo()->destructibleBuilding.rebuildingEvent)
+        StartEvents_Event(GetMap(), GetGOInfo()->destructibleBuilding.rebuildingEvent, this, pCaster->GetCharmerOrOwnerOrSelf());
 
     if (pCaster)
     {
@@ -2619,25 +2613,12 @@ void GameObject::TickCapturePoint()
                 players.insert(*itr);
         }
 
-        // Notify the battleground or outdoor pvp script
-        if (BattleGround* bg = (*capturingPlayers.begin())->GetBattleGround())
-        {
-            // Allow only certain events to be handled by other script engines
-            bg->HandleEvent(eventId, this);
-        }
-        else if (OutdoorPvP* outdoorPvP = sOutdoorPvPMgr.GetScript((*capturingPlayers.begin())->GetCachedZoneId()))
-        {
-            // Allow only certain events to be handled by other script engines
-            outdoorPvP->HandleEvent(eventId, this);
-        }
-
-        // Send script event to SD2 and database as well - this can be used for summoning creatures, casting specific spells or spawning GOs
-        if (!sScriptMgr.OnProcessEvent(eventId, this, this, true))
-            GetMap()->ScriptsStart(sEventScripts, eventId, this, this);
 
         DEBUG_LOG("GameObject::TickCapturePoint gameobject %s send event %u to faction %u, players group size %u, new state %u",
             GetObjectGuid().GetString().c_str(),
             eventId, progressFaction, players.size(), m_captureState);
+
+        StartEvents_Event(GetMap(), eventId, this, this, true, *capturingPlayers.begin());
     }
 }
 
