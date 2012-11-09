358f7c6812f50928c1c37b9f0226141e8973749d
rsa <kris13@bk.ru>
2012-11-09 20:45:41 +0400

    [mr2418] fix creatures 28614,28616

--- sql_mr/mr02418_mangos_creature_template_addon.sql
+++ sql_mr/mr02418_mangos_creature_template_addon.sql
@@ -5,3 +5,6 @@ DELETE FROM `creature_template_addon` WHERE `entry` = 19668;
 INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_pvp_state`, `emote`, `moveflags`, `auras`) VALUES
 (19668, 0, 0, 1, 40, 0, 0, '34429');
 
+-- Scarlet Gryphon
+DELETE FROM `creature_template_addon` WHERE `entry` IN (28614,28616);
+
