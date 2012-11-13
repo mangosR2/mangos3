68050112075e7f0cd024db573cd90301bd2b0637
rsa <kris13@bk.ru>
2012-11-14 00:33:02 +0400

    [mr2433] remove unneeded script call from MO_TRANSPORT

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -1391,9 +1391,6 @@ void GameObject::Use(Unit* user)
                 SetActiveObjectState(true);
             }
 
-            // activate script
-            if (!scriptReturnValue)
-                GetMap()->ScriptsStart(sGameObjectScripts, GetGUIDLow(), spellCaster, this);
             return;
         }
         case GAMEOBJECT_TYPE_FISHINGNODE:                   // 17 fishing bobber
