d9cc4a02efee425236f56f394f5a1a2b860b8b7a
Yaki Khadafi <elsoldollo@gmail.com>
2012-11-17 23:13:49 +0300

    [mt177] Fix shadowform visuals
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -12190,6 +12190,18 @@ void SpellAuraHolder::HandleSpellSpecificBoosts(bool apply)
                         spellId1 = 64134;                   // Body and Soul (periodic dispel effect)
                     break;
                 }
+                case 15473:                                 // Shadowform
+                {
+                    if (!apply)
+                    {
+                        spellId1 = 107903;
+                        spellId2 = 107904;
+                    }
+                    else
+                        // Glyph of Shadow alters Shadowform visual
+                        spellId1 = GetTarget()->HasAura(107906) ? 107904 : 107903;
+                    break;
+                }
                 // Dispersion mana reg and immunity
                 case 47585:
                     spellId1 = 60069;                       // Dispersion
