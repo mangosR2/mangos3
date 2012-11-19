40554e6e0ad51bc1bed9ec10513725f0b18190e0
Сергей AKA /dev/rsa <kris13@bk.ru>
2012-11-19 17:40:26 +0400

    Update src/game/LFG.h
    Fix problems with unsorted storage. Thx to Undergarun.

--- src/game/LFG.h
+++ src/game/LFG.h
@@ -221,7 +221,7 @@ enum LFGDungeonStatus
 };
 
 typedef std::set<LFGDungeonEntry const*> LFGDungeonSet;
-typedef UNORDERED_MAP<LFGDungeonEntry const*, LFGLockStatusType> LFGLockStatusMap;
+typedef std::map<LFGDungeonEntry const*, LFGLockStatusType> LFGLockStatusMap;
 
 /// Stores group data related to proposal to join
 struct LFGProposal
