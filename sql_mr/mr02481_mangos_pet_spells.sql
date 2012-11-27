91cf48528c284b7a55cd43dfd7cc9a65ee908fbd
rsa <kris13@bk.ru>
2012-11-27 10:28:36 +0400

    [mr2481] Use PetAI for DK gargoyle. also SD2 update.

--- sql_mr/mr02481_mangos_pet_spells.sql
+++ sql_mr/mr02481_mangos_pet_spells.sql
@@ -24,3 +24,10 @@ DELETE FROM `creature_spell` WHERE `guid` IN (31216);
 INSERT INTO `creature_spell` (`guid`, `spell`, `index`) VALUES
 (31216, 59637, 0),
 (31216, 59638, 1);
+
+-- DK Gargoyle
+UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = '27829';
+DELETE FROM `creature_spell` WHERE `guid` IN (27829);
+INSERT INTO `creature_spell` (`guid`, `spell`, `index`) VALUES
+(27829, 51963, 0),
+(27829, 43375, 1);
