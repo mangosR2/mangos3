7fd5b32ba0e4142193cf64d9d6cc2f15b1000eb8
Reamer <philipp.dallig@web.de>
2012-12-09 15:25:48 +0100

    [mr2514] doesn't add evade event when chase movement generator is finalize. finalize is called, when an other chase movement overrides the old one. this fix some mystic behaviour for example: when creature swap aggro target with petAI, defensiv mode and AI()->StartAtack(Unit*)

--- src/game/TargetedMovementGenerator.cpp
+++ src/game/TargetedMovementGenerator.cpp
@@ -274,8 +274,6 @@ template<class T>
 void ChaseMovementGenerator<T>::Finalize(T& owner)
 {
     owner.clearUnitState(UNIT_STAT_CHASE | UNIT_STAT_CHASE_MOVE);
-    if (owner.GetTypeId() == TYPEID_UNIT && owner.isAlive() && !owner.IsInEvadeMode())
-        owner.AddEvent(new EvadeDelayEvent(owner), EVADE_TIME_DELAY);
 }
 
 template<class T>
