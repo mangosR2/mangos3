8aa88ba215d0719c193875b2b3abd3c8b1ba850a
mns <pgameserver@gmail.com>
2012-12-25 03:21:44 +0330

    [mr2551] Clean up.

--- sql_mr/mr02551_mangos_temporary.sql
+++ sql_mr/mr02551_mangos_temporary.sql
@@ -16,5 +16,11 @@ UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = '27829';
 -- mr2523
 ALTER TABLE `spell_dbc` CHANGE `ProcCharges` `ProcCharges` INT(10) UNSIGNED NOT NULL DEFAULT 0;
 
+-- ---------- --
+-- 2012-12-25 --
+-- ---------- --
 -- mr2550
 UPDATE `creature_template` SET `IconName` = 'vehichleCursor' WHERE `IconName` = 'vehicleCursor';
+
+-- mr2551
+UPDATE `creature_template` SET PowerType = 3, `AIName` = 'NullAI' WHERE `entry` = 34045;
