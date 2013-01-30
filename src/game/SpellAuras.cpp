52ad603f70fd050ebbd82b75a95aec0048a1643f
Yaki Khadafi <elsoldollo@gmail.com>
2013-01-30 17:14:57 +0300

    [mt380] Improved Recuperate

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -9358,7 +9358,16 @@ void Aura::PeriodicTick()
                     damageInfo.damage = int32(amount * target->GetMaxHealth() / 100.0f);
                 // Recuperate
                 else if (spellProto->Id == 73651)
-                    damageInfo.damage = int32(amount * target->GetMaxHealth() / 100.0f);
+                {
+                    float healthPct = amount;
+                    if (target->GetTypeId() == TYPEID_PLAYER)
+                    {
+                        // Improved Recuperate
+                        if (SpellEntry const * spellInfo = ((Player*)target)->GetKnownTalentRankById(6395))
+                            healthPct += spellInfo->CalculateSimpleValue(EFFECT_INDEX_0) / 1000.0f;
+                    }
+                    damageInfo.damage = int32(healthPct * target->GetMaxHealth() / 100.0f);
+                }
             }
             damageInfo.CleanDamage(-int32(damageInfo.damage), 0, BASE_ATTACK, MELEE_HIT_NORMAL);
 
