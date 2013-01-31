698adc273b041cb36f3889968ce77208bbd58979
mns <pgameserver@gmail.com>
2013-02-01 01:56:13 +0330

    [mr2645] Fix typo.

--- sql_mr/mr02645_mangos_lunar_festival.sql
+++ sql_mr/mr02645_mangos_lunar_festival.sql
@@ -7,7 +7,7 @@
 DELETE FROM spell_script_target WHERE entry = 26373;
 INSERT INTO spell_script_target (entry, type, targetEntry) VALUES (26373, 0, 300058);
 
-DELETE FROM dbscripts_on_go_template_use WHERE entry = 300058;
+DELETE FROM dbscripts_on_go_template_use WHERE `id` = 300058;
 INSERT INTO dbscripts_on_go_template_use
   (id, delay, command, datalong, datalong2, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments)
 VALUES
