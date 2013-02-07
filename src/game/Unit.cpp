b48ca10dbc558119ef367c2b6585553ddab9dd94
boxa <boxa@mail.ru>
2013-02-08 03:32:09 +0400

    [mr2672] Use G3D calculation in GetCombatDistance

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7461,17 +7461,14 @@ void Unit::Uncharm()
     }
 }
 
-float Unit::GetCombatDistance( const Unit* target ) const
+float Unit::GetCombatDistance(const Unit* target) const
 {
     if (!target)
         return 0.0f;
 
-    float radius = target->GetFloatValue(UNIT_FIELD_COMBATREACH) + GetFloatValue(UNIT_FIELD_COMBATREACH);
-    float dx = GetPositionX() - target->GetPositionX();
-    float dy = GetPositionY() - target->GetPositionY();
-    float dz = GetPositionZ() - target->GetPositionZ();
-    float dist = sqrt((dx*dx) + (dy*dy) + (dz*dz)) - radius;
-    return ( dist > 0 ? dist : 0);
+    float radius = GetFloatValue(UNIT_FIELD_COMBATREACH) + target->GetFloatValue(UNIT_FIELD_COMBATREACH);
+    float dist = GetDistance(target->GetPosition()) - radius;
+    return (dist > M_NULL_F ? dist : 0.0f);
 }
 
 void Unit::SetPet(Pet* pet)
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "2671"
+ #define REVISION_R2 "2672"
 #endif // __REVISION_R2_H__
