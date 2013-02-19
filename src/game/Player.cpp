22622ece075906193328fa390d7e69505e2b6894
Xfurry <xfurry@scriptdev2.com>
2013-02-19 21:54:03 +0200

    [12361] Implement water script support for SSC water
    This requires additional support from SD2 side
    Also slightly improve performace on the UpdateunderwaterState function usage

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -413,6 +413,7 @@ Player::Player (WorldSession *session): Unit(), m_mover(this), m_camera(NULL), m
 
     m_zoneUpdateId = 0;
     m_zoneUpdateTimer = 0;
+    m_liquidUpdateTimer = 0;
 
     m_areaUpdateId = 0;
 
@@ -1330,6 +1331,14 @@ void Player::Update(uint32 update_diff, uint32 p_time)
             m_regenTimer = 0;
         else
             m_regenTimer -= update_diff;
+    }
+
+    if (m_liquidUpdateTimer)
+    {
+        if (update_diff >= m_liquidUpdateTimer)
+            m_liquidUpdateTimer = 0;
+        else
+            m_liquidUpdateTimer -= update_diff;
     }
 
     if (m_weaponChangeTimer > 0)
@@ -21816,6 +21825,12 @@ void Player::SetOriginalGroup(Group *group, int8 subgroup)
 
 void Player::UpdateUnderwaterState(Map* m, float x, float y, float z)
 {
+    // Update only on interval
+    if (m_liquidUpdateTimer)
+        return;
+    else
+        m_liquidUpdateTimer = 100;
+
     GridMapLiquidData liquid_status;
     GridMapLiquidStatus res = m->GetTerrain()->getLiquidStatus(x, y, z, MAP_ALL_LIQUIDS, &liquid_status);
     if (!res)
@@ -21823,10 +21838,10 @@ void Player::UpdateUnderwaterState(Map* m, float x, float y, float z)
         m_MirrorTimerFlags &= ~(UNDERWATER_INWATER | UNDERWATER_INLAVA | UNDERWATER_INSLIME | UNDERWATER_INDARKWATER);
         if (m_lastLiquid && m_lastLiquid->SpellId)
         {
-            RemoveAurasDueToSpell(m_lastLiquid->SpellId);
+            RemoveAurasDueToSpell(m_lastLiquid->SpellId == 37025 ? 37284 : m_lastLiquid->SpellId);
             if (GetVehicle() && GetVehicle()->GetBase())
             {
-                GetVehicle()->GetBase()->RemoveAurasDueToSpell(m_lastLiquid->SpellId);
+                GetVehicle()->GetBase()->RemoveAurasDueToSpell(m_lastLiquid->SpellId == 37025 ? 37284 : m_lastLiquid->SpellId);
             }
         }
         m_lastLiquid = NULL;
@@ -21849,21 +21864,39 @@ void Player::UpdateUnderwaterState(Map* m, float x, float y, float z)
 
         if (liquid && liquid->SpellId)
         {
+            // Exception for SSC water
+            uint32 liquidSpellId = liquid->SpellId == 37025 ? 37284 : liquid->SpellId;
+
             if (res & (LIQUID_MAP_UNDER_WATER | LIQUID_MAP_IN_WATER))
             {
-                if (SpellEntry const *pSpellEntry = sSpellStore.LookupEntry(liquid->SpellId))
+                if (SpellEntry const *pSpellEntry = sSpellStore.LookupEntry(liquidSpellId))
                 {
-                    // check aura for no double cast
+                    // check aura for no double cast (FIXME! seems as big hack...)
                     if (!HasAura(liquid->SpellId))
                     {
-                        if (SpellMgr::IsTargetMatchedWithCreatureType(pSpellEntry, this))
+                        if (InstanceData* pInst = GetInstanceData())
+                        {
+                            if (pInst->CheckConditionCriteriaMeet(this, INSTANCE_CONDITION_ID_LURKER, NULL, CONDITION_FROM_HARDCODED))
+                            {
+                                if (pInst->CheckConditionCriteriaMeet(this, INSTANCE_CONDITION_ID_SCALDING_WATER, NULL, CONDITION_FROM_HARDCODED))
+                                    CastSpell(this, liquidSpellId, true);
+                                else
+                                {
+                                    SummonCreature(21508, 0, 0, 0, 0, TEMPSUMMON_TIMED_OOC_DESPAWN, 2000);
+                                    // Special update timer for the SSC water
+                                    m_liquidUpdateTimer = 2000;
+                                }
+                            }
+                        }
+                        else if (SpellMgr::IsTargetMatchedWithCreatureType(pSpellEntry, this))
                         {
                             CastSpell(this, pSpellEntry, true);
                         }
+
                     }
                     if (GetVehicle() && GetVehicle()->GetBase())
                     {
-                        if (!GetVehicle()->GetBase()->HasAura(liquid->SpellId))
+                        if (!GetVehicle()->GetBase()->HasAura(liquidSpellId))
                         {
                             if (SpellMgr::IsTargetMatchedWithCreatureType(pSpellEntry, GetVehicle()->GetBase()))
                             {
@@ -21876,10 +21909,10 @@ void Player::UpdateUnderwaterState(Map* m, float x, float y, float z)
             else
             {
                 // Player is above Water or walk on Water
-                RemoveAurasDueToSpell(liquid->SpellId);
+                RemoveAurasDueToSpell(liquidSpellId);
                 if (GetVehicle() && GetVehicle()->GetBase())
                 {
-                    GetVehicle()->GetBase()->RemoveAurasDueToSpell(liquid->SpellId);
+                    GetVehicle()->GetBase()->RemoveAurasDueToSpell(liquidSpellId);
                 }
             }
         }
@@ -21889,11 +21922,12 @@ void Player::UpdateUnderwaterState(Map* m, float x, float y, float z)
     else if (m_lastLiquid && m_lastLiquid->SpellId)
     {
         // Player change to no specific LiquidEntry (= 0)
-        RemoveAurasDueToSpell(m_lastLiquid->SpellId);
+        RemoveAurasDueToSpell(m_lastLiquid->SpellId == 37025 ? 37284 : m_lastLiquid->SpellId);
         if (GetVehicle() && GetVehicle()->GetBase())
         {
-            GetVehicle()->GetBase()->RemoveAurasDueToSpell(m_lastLiquid->SpellId);
+            GetVehicle()->GetBase()->RemoveAurasDueToSpell(m_lastLiquid->SpellId == 37025 ? 37284 : m_lastLiquid->SpellId);
         }
+        m_lastLiquid = NULL;
     }
 
     // All liquids type - check under water position
