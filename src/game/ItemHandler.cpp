462f3ba267a1b73c04404117aa05e6e9b2696157
Dramacydal <PulLumBerMal@gmail.com>
2013-02-20 17:07:42 +0300

    [c12474] More generic handling for meta currencies
    Signed-off-by: Dramacydal <PulLumBerMal@gmail.com>

--- src/game/ItemHandler.cpp
+++ src/game/ItemHandler.cpp
@@ -654,7 +654,7 @@ void WorldSession::SendListInventory(ObjectGuid vendorguid)
                 if (!pCurrency)
                     continue;
 
-                if (pCurrency->ID == CURRENCY_CONQUEST_ARENA_META || pCurrency->ID == CURRENCY_CONQUEST_BG_META)
+                if (pCurrency->Category == CURRENCY_CATEGORY_META)
                     continue;
 
                 ++count;
