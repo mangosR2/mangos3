54dd11ef5cb0b451516be2220d31f0ea5e6ceef1
Dramacydal <PulLumBerMal@gmail.com>
2013-02-24 00:48:12 +0300

    [c12509] Implement SPELLMOD_SPELL_COST_REFUND_ON_FAIL
    Based on TrinityCore, author is unknown
    
    Signed-off-by: Dramacydal <PulLumBerMal@gmail.com>
    
    Conflicts:
    	src/game/Unit.h
    	src/shared/revision_nr.h

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,8 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
+<<<<<<< HEAD
  #define REVISION_NR "12499"
+=======
+ #define REVISION_NR "12509"
+>>>>>>> de32ef7... [c12509] Implement SPELLMOD_SPELL_COST_REFUND_ON_FAIL
 #endif // __REVISION_NR_H__
