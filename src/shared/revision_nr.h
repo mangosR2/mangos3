e90fbc180e013982748c6f11577e99f1f807b0e6
rsa <kris13@bk.ru>
2013-03-05 21:10:56 +0400

    compile fix

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
-<<<<<<< HEAD
- #define REVISION_NR "12397"
-=======
  #define REVISION_NR "12399"
->>>>>>> dfcccf4... [12399] Add support for spell 62941
 #endif // __REVISION_NR_H__
