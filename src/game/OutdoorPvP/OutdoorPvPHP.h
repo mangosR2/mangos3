9add712de260d9c87e542fee8b69f9bf16b225ad
Yaki Khadafi <elsoldollo@gmail.com>
2013-05-02 17:58:45 +0300

    [mt680] Changes honor amounts on Hellfire and Silithus

--- src/game/OutdoorPvP/OutdoorPvPHP.h
+++ src/game/OutdoorPvP/OutdoorPvPHP.h
@@ -39,7 +39,7 @@ enum
     NPC_CAPTURE_CREDIT_BROKEN_HILL          = 19032,
 
     // misc
-    HONOR_REWARD_HELLFIRE                   = 18,
+    HONOR_REWARD_HELLFIRE                   = 1800,
 
     // gameobjects
     GO_TOWER_BANNER_OVERLOOK                = 182525,
