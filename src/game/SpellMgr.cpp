da9615a34d09a6b23e089bc8d9ab552fd08591a5
Yaki Khadafi <elsoldollo@gmail.com>
2013-05-05 22:03:40 +0300

    [mt682] Possible crashfix in SpellMgr::IsSpellProcEventCanTriggeredBy

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -2214,7 +2214,7 @@ bool SpellMgr::IsSpellProcEventCanTriggeredBy(SpellProcEventEntry const * spellP
             SpellClassOptionsEntry const* spellClassOptions = procSpell->GetSpellClassOptions();
 
             // Check (if set) for spellFamilyName
-            if (spellProcEvent->spellFamilyName && (!spellClassOptions && spellProcEvent->spellFamilyName != SPELLFAMILY_GENERIC || spellProcEvent->spellFamilyName != spellClassOptions->SpellFamilyName))
+            if (spellProcEvent->spellFamilyName && (!spellClassOptions && spellProcEvent->spellFamilyName != SPELLFAMILY_GENERIC || spellClassOptions && spellProcEvent->spellFamilyName != spellClassOptions->SpellFamilyName))
                 return false;
         }
     }
