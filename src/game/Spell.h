f029baa6bff49fb4ead5ea2e38f00c9a0b798f4d
Sidsukana <pgsilent@gmail.com>
2013-05-10 14:42:50 +0200

    [12510] Fix take ammo for most ranged spells
    Fix spells like Arcane Shot not taking ammo while they should
    
    Close cmangos/mangos-tbc#19
    
    Signed-off-by: cala <calaftp@free.fr>
    Signed-off-by: Schmoozerd <schmoozerd@cmangos.net>

--- src/game/Spell.h
+++ src/game/Spell.h
@@ -395,6 +395,7 @@ class Spell
         void cast(bool skipCheck = false);
         void finish(bool ok = true);
         void TakePower();
+        void TakeAmmo();
         void TakeReagents();
         void TakeCastItem();
 
