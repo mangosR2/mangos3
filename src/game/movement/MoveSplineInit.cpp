03489165ea2beca7b52fad26a6a983716880ca05
boxa <boxa@mail.ru>
2013-06-08 10:39:15 +0400

    [mr2819] Do not add MOVEFLAG_WALK_MODE to creatures that did not have it but called SetWalk() for their movement
    picked from TC, author Shauren.
    seems logical
    
    TESTING

--- src/game/movement/MoveSplineInit.cpp
+++ src/game/movement/MoveSplineInit.cpp
@@ -85,21 +85,24 @@ namespace Movement
         args.initialOrientation = real_position.orientation;
 
         uint32 moveFlags = unit.m_movementInfo.GetMovementFlags();
-        if (args.flags.walkmode)
-            moveFlags |= MOVEFLAG_WALK_MODE;
-        else
-            moveFlags &= ~MOVEFLAG_WALK_MODE;
-
         moveFlags |= (MOVEFLAG_SPLINE_ENABLED | MOVEFLAG_FORWARD);
-
-        if (fabs(args.velocity) < M_NULL_F)
-            args.velocity = unit.GetSpeed(SelectSpeedType(moveFlags));
-
-        if (!args.Validate(&unit))
-            return 0;
 
         if (moveFlags & MOVEFLAG_ROOT)
             moveFlags &= ~MOVEFLAG_MASK_MOVING;
+
+        if (fabs(args.velocity) < M_NULL_F)
+        {
+            // If spline is initialized with SetWalk method it only means we need to select
+            // walk move speed for it but not add walk flag to unit
+            uint32 moveFlagsForSpeed = moveFlags;
+            if (args.flags.walkmode)
+                moveFlagsForSpeed |= MOVEFLAG_WALK_MODE;
+
+            args.velocity = unit.GetSpeed(SelectSpeedType(moveFlagsForSpeed));
+        }
+
+        if (!args.Validate(&unit))
+            return 0;
 
         unit.m_movementInfo.SetMovementFlags((MovementFlags)moveFlags);
         move_spline.Initialize(args);
