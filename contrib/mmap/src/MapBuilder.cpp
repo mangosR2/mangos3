eef6964ea18e9c9a5953c5729f1eb4a075e3e62c
boxa <boxa@mail.ru>
2013-07-04 20:44:52 +0400

    [mr2836] Tune MMapBuilder for more realistic path generation

--- contrib/mmap/src/MapBuilder.cpp
+++ contrib/mmap/src/MapBuilder.cpp
@@ -420,10 +420,10 @@ namespace MMAP
         config.borderSize = config.walkableRadius + 3;
         config.maxEdgeLen = VERTEX_PER_TILE + 1;        //anything bigger than tileSize
         config.walkableHeight = m_bigBaseUnit ? 3 : 6;
-        config.walkableClimb = m_bigBaseUnit ? 2 : 4;   // keep less than walkableHeight
+        config.walkableClimb = m_bigBaseUnit ? 1 : 2;   // keep less than walkableHeight
         config.minRegionArea = rcSqr(60);
         config.mergeRegionArea = rcSqr(50);
-        config.maxSimplificationError = 2.0f;       // eliminates most jagged edges (tinny polygons)
+        config.maxSimplificationError = 1.8f;       // eliminates most jagged edges (tinny polygons)
         config.detailSampleDist = config.cs * 64;
         config.detailSampleMaxError = config.ch * 2;
 
