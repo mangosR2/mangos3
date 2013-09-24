a1e0589227bd8208bf87180f3742b146f061f80f
Undergarun <undergarun@gmail.com>
2013-09-24 03:36:38 +0200

    [mr2967] Remove not needed anymore fix for creature spawndist field. Already have proper format on base ddbb.

--- sql_mr/mr00767_mangos_creature_table_fix.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `creature` CHANGE `spawndist` `spawndist` FLOAT DEFAULT '0' NOT NULL;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "2966"
+ #define REVISION_R2 "2967"
 #endif // __REVISION_R2_H__
