64e55d29dadda03c98cd9d736f863bc5a1a96538
boxa <boxa@mail.ru>
2013-10-05 20:12:55 +0400

    [mr2985] Group: extends requests about member role

--- src/game/Group.h
+++ src/game/Group.h
@@ -123,15 +123,18 @@ enum GroupFlagMask
     GROUP_MAIN_TANK      = (1 << GROUP_FLAG_MAIN_TANK),
 
     // unions
-    GROUP_MEMBER_AMT     = ( GROUP_ASSISTANT   |
-                             GROUP_MAIN_ASSISTANT |
-                             GROUP_MAIN_TANK      ),
-    GROUP_MEMBER_AT      = ( GROUP_ASSISTANT   |
-                             GROUP_MAIN_TANK      ),
-    GROUP_MEMBER_AM      = ( GROUP_ASSISTANT   |
-                             GROUP_MAIN_ASSISTANT ),
-    GROUP_MEMBER_MT      = ( GROUP_MAIN_ASSISTANT |
-                             GROUP_MAIN_TANK      ),
+    GROUP_MEMBER_AMT     = (GROUP_ASSISTANT |
+                            GROUP_MAIN_ASSISTANT |
+                            GROUP_MAIN_TANK),
+
+    GROUP_MEMBER_AT      = (GROUP_ASSISTANT |
+                            GROUP_MAIN_TANK),
+
+    GROUP_MEMBER_AM      = (GROUP_ASSISTANT |
+                            GROUP_MAIN_ASSISTANT),
+
+    GROUP_MEMBER_MT      = (GROUP_MAIN_ASSISTANT |
+                            GROUP_MAIN_TANK),
 
 };
 
@@ -280,20 +283,28 @@ class MANGOS_DLL_SPEC Group
         bool IsLeader(ObjectGuid guid) const { return GetLeaderGuid() == guid; }
         ObjectGuid GetMemberGuid(const std::string& name)
         {
-            for(member_citerator itr = m_memberSlots.begin(); itr != m_memberSlots.end(); ++itr)
+            for (member_citerator itr = m_memberSlots.begin(); itr != m_memberSlots.end(); ++itr)
+            {
                 if (itr->name == name)
                     return itr->guid;
-
+            }
             return ObjectGuid();
         }
-        bool IsAssistant(ObjectGuid guid) const
+        bool IsGroupRole(ObjectGuid const& guid, GroupFlagMask role) const
         {
             member_citerator mslot = _getMemberCSlot(guid);
-            if (mslot==m_memberSlots.end())
+            if (mslot == m_memberSlots.end())
                 return false;
 
-            return mslot->flags & GROUP_ASSISTANT;
+            if (role == GROUP_MEMBER)
+                return true;
+            else
+                return mslot->flags & role;
         }
+        bool IsMainAssistant(ObjectGuid const& guid) const { return IsGroupRole(guid, GROUP_MAIN_ASSISTANT); }
+        bool IsMainTank(ObjectGuid const& guid) const { return IsGroupRole(guid, GROUP_MAIN_TANK); }
+        bool IsAssistant(ObjectGuid const& guid) const { return IsGroupRole(guid, GROUP_ASSISTANT); }
+
         Player* GetInvited(ObjectGuid guid) const;
         Player* GetInvited(const std::string& name) const;
 
