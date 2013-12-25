fa270568bb2f0d6af3e105876706d3e277c1d189
Cyberium <cybermomo@techemail.com>
2013-12-25 14:53:40 +0100

    [12584] Rename m_respawnAggroDelay to make it more generic

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -140,7 +140,7 @@ Creature::Creature(CreatureSubtype subtype) : Unit(),
     loot(this),
     lootForPickPocketed(false), lootForBody(false), lootForSkin(false),
     m_lootMoney(0),
-    m_corpseDecayTimer(0), m_respawnTime(0), m_respawnDelay(25), m_corpseDelay(60), m_respawnradius(5.0f), m_respawnAggroDelay(0),
+    m_corpseDecayTimer(0), m_respawnTime(0), m_respawnDelay(25), m_corpseDelay(60), m_respawnradius(5.0f), m_aggroDelay(0),
     m_subtype(subtype), m_defaultMovementType(IDLE_MOTION_TYPE), m_equipmentId(0),
     m_AlreadyCallAssistance(false), m_AlreadySearchedAssistance(false),
     m_regenHealth(true), m_AI_locked(false), m_isDeadByDefault(false),
@@ -467,7 +467,7 @@ void Creature::Update(uint32 update_diff, uint32 diff)
             {
                 DEBUG_FILTER_LOG(LOG_FILTER_AI_AND_MOVEGENSS, "Respawning...");
                 m_respawnTime = 0;
-                m_respawnAggroDelay = sWorld.getConfig(CONFIG_UINT32_CREATURE_RESPAWN_AGGRO_DELAY);
+                m_aggroDelay = sWorld.getConfig(CONFIG_UINT32_CREATURE_RESPAWN_AGGRO_DELAY);
                 lootForPickPocketed = false;
                 lootForBody         = false;
                 lootForSkin         = false;
@@ -541,13 +541,10 @@ void Creature::Update(uint32 update_diff, uint32 diff)
         }
         case ALIVE:
         {
-            if (m_respawnAggroDelay)
-            {
-                if (m_respawnAggroDelay <= update_diff)
-                    m_respawnAggroDelay = 0;
-                else
-                    m_respawnAggroDelay -= update_diff;
-            }
+            if (m_aggroDelay <= update_diff)
+                m_aggroDelay = 0;
+            else
+                m_aggroDelay -= update_diff;
 
             if (m_isDeadByDefault)
             {
@@ -1886,7 +1883,7 @@ bool Creature::CanInitiateAttack()
     if (isPassiveToHostile())
         return false;
 
-    if (m_respawnAggroDelay != 0)
+    if (m_aggroDelay != 0)
         return false;
 
     return true;
