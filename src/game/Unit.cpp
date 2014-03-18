06e17310c62f8ca0aabfbb0f62e7b5ecbf7ff0b1
Undergarun <undergarun@gmail.com>
2014-03-18 14:39:27 +0100

    [mt419] Death Knight - Necrotic Strike

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11805,6 +11805,13 @@ int32 Unit::CalculateSpellDamage(Unit const* target, SpellEntry const* spellProt
             }
             break;
         }
+        case SPELL_AURA_HEAL_ABSORB:
+        {
+            // Necrotic Strike
+            if (spellProto->Id == 73975)
+                return int32(GetTotalAttackPowerValue(BASE_ATTACK) * 0.7f);
+            break;
+        }
     }
 
     Player* unitPlayer = (GetTypeId() == TYPEID_PLAYER) ? (Player*)this : NULL;
