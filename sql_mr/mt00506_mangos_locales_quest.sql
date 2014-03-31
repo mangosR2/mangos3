b5657d58a62de043f8f4d9210cfa59685fa3289c
Undergarun <undergarun@gmail.com>
2014-03-31 14:16:38 +0200

    [mt506] Rename quest locales to better naming.

---
+++ sql_mr/mt00506_mangos_locales_quest.sql
@@ -0,0 +1,51 @@
+ALTER TABLE `locales_quest`
+    CHANGE `PortraitGiverName_loc1` `QuestGiverPortraitName_loc1` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc2` `QuestGiverPortraitName_loc2` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc3` `QuestGiverPortraitName_loc3` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc4` `QuestGiverPortraitName_loc4` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc5` `QuestGiverPortraitName_loc5` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc6` `QuestGiverPortraitName_loc6` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc7` `QuestGiverPortraitName_loc7` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc8` `QuestGiverPortraitName_loc8` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc9` `QuestGiverPortraitName_loc9` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc10` `QuestGiverPortraitName_loc10` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverName_loc11` `QuestGiverPortraitName_loc11` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+
+ALTER TABLE `locales_quest`
+    CHANGE `PortraitGiverText_loc1` `QuestGiverPortraitText_loc1` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc2` `QuestGiverPortraitText_loc2` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc3` `QuestGiverPortraitText_loc3` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc4` `QuestGiverPortraitText_loc4` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc5` `QuestGiverPortraitText_loc5` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc6` `QuestGiverPortraitText_loc6` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc7` `QuestGiverPortraitText_loc7` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc8` `QuestGiverPortraitText_loc8` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc9` `QuestGiverPortraitText_loc9` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc10` `QuestGiverPortraitText_loc10` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitGiverText_loc11` `QuestGiverPortraitText_loc11` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+
+ALTER TABLE `locales_quest`
+    CHANGE `PortraitTurnInName_loc1` `QuestTurnInPortraitName_loc1` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc2` `QuestTurnInPortraitName_loc2` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc3` `QuestTurnInPortraitName_loc3` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc4` `QuestTurnInPortraitName_loc4` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc5` `QuestTurnInPortraitName_loc5` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc6` `QuestTurnInPortraitName_loc6` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc7` `QuestTurnInPortraitName_loc7` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc8` `QuestTurnInPortraitName_loc8` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc9` `QuestTurnInPortraitName_loc9` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc10` `QuestTurnInPortraitName_loc10` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInName_loc11` `QuestTurnInPortraitName_loc11` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+
+ALTER TABLE `locales_quest`
+    CHANGE `PortraitTurnInText_loc1` `QuestTurnInPortraitText_loc1` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc2` `QuestTurnInPortraitText_loc2` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc3` `QuestTurnInPortraitText_loc3` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc4` `QuestTurnInPortraitText_loc4` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc5` `QuestTurnInPortraitText_loc5` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc6` `QuestTurnInPortraitText_loc6` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc7` `QuestTurnInPortraitText_loc7` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc8` `QuestTurnInPortraitText_loc8` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc9` `QuestTurnInPortraitText_loc9` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc10` `QuestTurnInPortraitText_loc10` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL,
+    CHANGE `PortraitTurnInText_loc11` `QuestTurnInPortraitText_loc11` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
