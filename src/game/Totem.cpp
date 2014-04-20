1036ad9855ab8e60d274100846fad490d5916c2c
rsa <kris13@bk.ru>
2014-04-20 08:27:29 +0400

    [mr3214] Totem are immune to their own spell workaround fix  (by motives of cmangos/12630)

--- src/game/Totem.cpp
+++ src/game/Totem.cpp
@@ -215,6 +215,13 @@ void Totem::SetTypeBySummonSpell(SpellEntry const * spellProto)
 
 bool Totem::IsImmuneToSpellEffect(SpellEntry const* spellInfo, SpellEffectIndex index) const
 {
+    if (!spellInfo)
+        return true;
+
+    // Author - Velvet. seems not fully correct, but usable.
+    if (spellInfo->GetSpellFamilyFlags().test<CF_SHAMAN_MANA_SPRING, CF_SHAMAN_HEALING_STREAM, CF_SHAMAN_MISC_TOTEM_EFFECTS>())
+        return false;
+
     switch(spellInfo->Effect[index])
     {
         case SPELL_EFFECT_ATTACK_ME:
