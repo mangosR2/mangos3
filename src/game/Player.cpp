f7eaac2e8da592e4217b3025d5b19366385a84d8
Undergarun <undergarun@gmail.com>
2014-04-30 10:50:40 +0200

    [mt944] Attempt to fix crash in HandleFall with Transports. ty Dramacydal

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -23607,7 +23607,7 @@ void Player::HandleFall(MovementInfo const& movementInfo)
     // 14.57 can be calculated by resolving damageperc formula below to 0
     if (z_diff >= 14.57f && !isDead() && !isGameMaster() &&
         !HasAuraType(SPELL_AURA_HOVER) && !HasAuraType(SPELL_AURA_FEATHER_FALL) &&
-        !HasAuraType(SPELL_AURA_FLY) && !IsImmunedToDamage(SPELL_SCHOOL_MASK_NORMAL) && !hasFlyMountAura && GetTransportInfo()->GetTransportGuid().IsEmpty())
+        !HasAuraType(SPELL_AURA_FLY) && !IsImmunedToDamage(SPELL_SCHOOL_MASK_NORMAL) && !hasFlyMountAura && (!GetTransportInfo() || GetTransportInfo()->GetTransportGuid().IsEmpty()))
     {
         // Safe fall, fall height reduction
         int32 safe_fall = GetTotalAuraModifier(SPELL_AURA_SAFE_FALL);
