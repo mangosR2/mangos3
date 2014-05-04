7eae9f8970f176ac870cc7267c051b0fcc85efc1
Undergarun <undergarun@gmail.com>
2014-05-04 07:43:16 +0200

    [mt973] Rename variable to fit changes in StormLib

--- contrib/vmap_extractor/vmapextract/mpqfile.cpp
+++ contrib/vmap_extractor/vmapextract/mpqfile.cpp
@@ -10,7 +10,7 @@ MPQFile::MPQFile(HANDLE mpq, const char* filename):
     size(0)
 {
     HANDLE file = NULL;
-    if (!SFileOpenFileEx(mpq, filename, SFILE_OPEN_PATCHED_FILE, &file))
+    if (!SFileOpenFileEx(mpq, filename, SFILE_OPEN_FROM_MPQ, &file))
     {
         int _err = GetLastError();
         if (_err != ERROR_FILE_NOT_FOUND)
