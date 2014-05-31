d2a9854244bed31490ee6f4ac9ea07219e4c0184
Undergarun <undergarun@gmail.com>
2014-05-31 07:27:14 +0200

    [mr3234] Cleanup in SQL and remove tables included on custom_mangos_tables

--- sql_mr/mr01792_mangos_spell_linked.sql
+++ sql_mr/mr01792_mangos_spell_linked.sql
@@ -1,12 +1,4 @@
 -- Implement spell linked definitions storage
-CREATE TABLE IF NOT EXISTS `spell_linked` (
-    `entry`            int(10) unsigned NOT NULL COMMENT 'Spell entry',
-    `linked_entry`     int(10) unsigned NOT NULL COMMENT 'Linked spell entry',
-    `type`             int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Type of link',
-    `effect_mask`      int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'mask of effect (NY)',
-    `comment`          varchar(255) NOT NULL DEFAULT '',
-     PRIMARY KEY (`entry`,`linked_entry`,`type`)
-) DEFAULT CHARSET=utf8 PACK_KEYS=0 COMMENT='Linked spells storage';
 
 DELETE FROM `spell_linked` WHERE `entry` = 25780;
 INSERT INTO `spell_linked` (`entry`, `linked_entry`, `type`, `effect_mask`, `comment`) VALUES
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "3233"
+ #define REVISION_R2 "3234"
 #endif // __REVISION_R2_H__
