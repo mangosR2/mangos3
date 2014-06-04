1d0d0108577ea5ee9b2433152fa4ad69d52643fa
Undergarun <undergarun@gmail.com>
2014-06-04 05:23:52 +0200

    [mr3239] Output creature entry on spawnMask error output.

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -1428,7 +1428,7 @@ void ObjectMgr::LoadCreatures()
         }
 
         if (data.spawnMask & ~spawnMasks[data.mapid])
-            sLog.outErrorDb("Table `creature` have creature (GUID: %u) that have wrong spawn mask %u including not supported difficulty modes for map (Id: %u).",guid, data.spawnMask, data.mapid );
+            sLog.outErrorDb("Table `creature` have creature (GUID: %u Entry: %u) that have wrong spawn mask %u including not supported difficulty modes for map (Id: %u).", guid, entry, data.spawnMask, data.mapid);
 
         bool ok = true;
         for (uint32 diff = 0; diff < MAX_DIFFICULTY - 1 && ok; ++diff)
