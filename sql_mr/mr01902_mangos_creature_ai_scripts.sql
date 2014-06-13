66143952a7013686c17f880532c24b417147bfc0
Undergarun <undergarun@gmail.com>
2014-06-13 10:01:28 +0200

    [mr3245] Allow to execute mangos_strings on both table versions. (For mangosR2 and mangos3). Also added missing creature_template change.

--- sql_mr/mr01902_mangos_creature_ai_scripts.sql
+++ sql_mr/mr01902_mangos_creature_ai_scripts.sql
@@ -1,3 +1,3 @@
 -- Friend of Fowl achieve
 UPDATE `creature_ai_scripts` SET `action1_param3` = 3 WHERE `id` IN(2380101, 2474601);
-UPDATE `creature_template` SET `type` = 1 WHERE `entry` IN(23801, 24746);
+UPDATE `creature_template` SET `CreatureType` = 1 WHERE `entry` IN(23801, 24746);
