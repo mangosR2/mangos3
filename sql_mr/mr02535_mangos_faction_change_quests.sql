78e2f2119cf565c03e19f5a69c11028aee2668b3
Undergarun <undergarun@gmail.com>
2014-06-13 10:03:16 +0200

    [mt1058] Remove obsolete fixes in tables. DB providers now have right data. Also enable one quest in faction change process.

--- sql_mr/mr02535_mangos_faction_change_quests.sql
+++ sql_mr/mr02535_mangos_faction_change_quests.sql
@@ -4,5 +4,5 @@ INSERT INTO player_factionchange_quests VALUES (12743, 'A Special Surprise(A)', 
 INSERT INTO player_factionchange_quests VALUES (12744, 'A Special Surprise(A)', 12748, 'A Special Surprise(H)');
 INSERT INTO player_factionchange_quests VALUES (12745, 'A Special Surprise(A)', 12749, 'A Special Surprise(H)');
 INSERT INTO player_factionchange_quests VALUES (12746, 'A Special Surprise(A)', 12750, 'A Special Surprise(H)');
--- INSERT INTO player_factionchange_quests VALUES (28649, 'A Special Surprise(A)', 28650, 'A Special Surprise(H)'); Uncomment on Cataclysm
+INSERT INTO player_factionchange_quests VALUES (28649, 'A Special Surprise(A)', 28650, 'A Special Surprise(H)');
 INSERT INTO player_factionchange_quests VALUES (13188, 'Where Kings Walk', 13189, 'Warchief''s Blessing');
--- sql_mr/mt00083_mangos_npc_vendor.sql
+++
@@ -1,3 +0,0 @@
--- Correct tables
-ALTER TABLE `npc_vendor_template` ADD COLUMN `condition_id` mediumint(8) unsigned NOT NULL default '0' AFTER ExtendedCost;
-ALTER TABLE `npc_vendor` ADD COLUMN `condition_id` mediumint(8) unsigned NOT NULL default '0' AFTER ExtendedCost;
