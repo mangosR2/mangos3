8f9f93bdf5f7c91e1bfa983299fc6282ed8e77a6
Undergarun <undergarun@gmail.com>
2014-06-30 05:31:32 +0200

    [mr1062] Some data fields from gameobject_template can be unsigned values.

---
+++ sql_mr/mt01062_mangos_gameobject_template.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `gameobject_template`
+    CHANGE `data1` `data1` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data6` `data6` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data24` `data24` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data25` `data25` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data26` `data26` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data27` `data27` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data28` `data28` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data29` `data29` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data30` `data30` INT(10) DEFAULT 0 NOT NULL,
+    CHANGE `data31` `data31` INT(10) DEFAULT 0 NOT NULL;
